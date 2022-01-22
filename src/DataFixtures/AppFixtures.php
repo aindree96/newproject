acc402a87654943be4acbb362c857a6064caa0c7
Aindree Ghosh <aindree.ghosh@einzigtech.com>
2022-01-22 20:00:13 +0530

    Hasing password directly from Appfixtures without using factory

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,17 +3,30 @@
 namespace App\DataFixtures;
 
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Doctrine\Persistence\ObjectManager;
 use App\Entity\User;
 use App\Factory\UserFactory;
 
 class AppFixtures extends Fixture
 {
+
+    private UserPasswordHasherInterface $hasher;
+
+    public function __construct(UserPasswordHasherInterface $hasher)
+    {
+        $this->hasher = $hasher;
+    }
+
     public function load(ObjectManager $manager): void
     {
-        UserFactory::createOne(['email'=>'admin@gmail.com',
-            'roles'=>["ROLE_ADMIN"]
-        ]);
-        $manager->flush();
+       $user=new User();
+       $user->setEmail('admin@gmail.com');
+       $user->setRoles(['ROLE_ADMIN']);
+
+       $password = $this->hasher->hashPassword($user, 'admina');
+       $user->setPassword($password);
+       $manager->persist($user);
+       $manager->flush();
     }
 }
